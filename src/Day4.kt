4d91470633f765cd05bfb1675d07e91a443f3ca6
andreas-umbricht <andreas.umbricht@gmail.com>
2025-12-04 10:05:15 +0100

    Day 4 puzzle 2

--- src/Day4.kt
+++ src/Day4.kt
@@ -1,9 +1,66 @@
 import java.io.BufferedReader
 import java.io.File
+import kotlin.text.getOrNull
 
 fun main() {
     val input = getInput()
 
+    val passcode1 = puzzle1(input)
+    val passcode2 = puzzle2(input)
+
+    println("Passcode1: $passcode1")
+    println("Paccode2: $passcode2")
+}
+
+private fun puzzle2(input: List<String>): Int {
+    val places = input.map { it -> it.map { Place(char = it) } }
+
+    places.forEachIndexed { index, row ->
+        row.forEachIndexed { rowIndex, place ->
+            place.addNeighbour(row.getOrNull(rowIndex + 1))
+            place.addNeighbour(row.getOrNull(rowIndex - 1))
+            place.addNeighbour(places.getOrNull(index - 1)?.getOrNull(rowIndex - 1))
+            place.addNeighbour(places.getOrNull(index - 1)?.getOrNull(rowIndex))
+            place.addNeighbour(places.getOrNull(index - 1)?.getOrNull(rowIndex + 1))
+            place.addNeighbour(places.getOrNull(index + 1)?.getOrNull(rowIndex - 1))
+            place.addNeighbour(places.getOrNull(index + 1)?.getOrNull(rowIndex))
+            place.addNeighbour(places.getOrNull(index + 1)?.getOrNull(rowIndex + 1))
+        }
+    }
+
+    return places.flatten().sumOf { it.check() }
+}
+
+private class Place(
+    val char: Char,
+) {
+    private var neighbours = mutableListOf<Place>()
+
+    var destroyed = false
+        private set
+
+    fun addNeighbour(n: Place?) {
+        n?.let { if (n.char == '@') neighbours.add(n) }
+    }
+
+    fun check(): Int {
+        return if (neighbours.size < 4 && !destroyed && char == '@') {
+            destroyed = true
+            neighbours.sumOf { it.removeNeighbour(this) } + 1
+        } else {
+            0
+        }
+    }
+
+    fun removeNeighbour(place: Place): Int {
+        if (destroyed) return 0
+
+        neighbours.remove(place)
+        return check()
+    }
+}
+
+private fun puzzle1(input: List<String>): Int {
     var result = 0
 
     input.forEachIndexed { rowIndex, row ->
@@ -31,7 +88,7 @@ fun main() {
         }
     }
 
-    println("Passcode: $result")
+    return result
 }
 
 private fun checkAdjacentRow(adjacentRow: String?, charIndex: Int): Int {

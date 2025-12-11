1f89fae81604ea848eb7f6644ce52c7ef00eafd7
andreas-umbricht <andreas.umbricht@gmail.com>
2025-12-11 23:04:15 +0100

    Day 8

--- src/Day8.kt
+++ src/Day8.kt
@@ -1,5 +1,6 @@
 import java.io.BufferedReader
 import java.io.File
+import javax.swing.Box
 import kotlin.math.sqrt
 import kotlin.text.toInt
 
@@ -7,50 +8,87 @@ fun main() {
     val input = getInput()
 
     val passCode1 = solve1(input)
+    val passCode2 = solve2(input)
 
     println("Passcode1: $passCode1")
+    println("Passcode2: $passCode2")
 }
 
-private fun solve1(input: List<String>): Int {
-    val boxes = input.map {
-        it.split(",").let {
-            JunctionBox(
-                x = it[0].toInt(),
-                y = it[1].toInt(),
-                z = it[2].toInt()
-            )
-        }
-    }.toMutableList()
+private fun solve2(input: List<String>): Long {
+    val boxes = getBoxes(input)
+    val connections = getClosestConnections(boxes)
 
-    var connections = mutableListOf<Connection>()
+    val circuits = boxes.associateWith { Circuit(boxes = listOf(it)) }.toMutableMap()
+
+    var lastConnection: Connection? = null
+
+    var i = 0
+
+    while(lastConnection == null) {
+        val connection = connections[i]
+
+        if (circuits[connection.to] != circuits[connection.from]) {
+            val newCircuit = Circuit(
+                boxes = (circuits[connection.to]!!.boxes + circuits[connection.from]!!.boxes).distinct()
+            )
+
+            if (newCircuit.boxes.size == boxes.size) lastConnection = connection
+
+            newCircuit.boxes.forEach {
+                circuits[it] = newCircuit
+            }
+        }
+
+        i++
+    }
+
+    return lastConnection.from.x * lastConnection.to.x
+}
+
+private fun solve1(input: List<String>): Long {
+    val boxes = getBoxes(input)
+    val connections = getClosestConnections(boxes)
+
+    val circuits = boxes.associateWith { Circuit(boxes = listOf(it)) }.toMutableMap()
+
+    repeat(1000) {
+        val connection = connections[it]
+
+        if (circuits[connection.to] != circuits[connection.from]) {
+            val newCircuit = Circuit(
+                boxes = (circuits[connection.to]!!.boxes + circuits[connection.from]!!.boxes).distinct()
+            )
+
+            newCircuit.boxes.forEach {
+                circuits[it] = newCircuit
+            }
+        }
+    }
+
+    return circuits.values.distinct().sortedByDescending { it.boxes.size }.take(3).fold(1) { acc, circuit -> acc * circuit.boxes.size }
+}
+
+private fun getClosestConnections(boxes: List<JunctionBox>): MutableList<Connection> {
+    val connections = mutableListOf<Connection>()
 
     boxes.forEachIndexed { index1, box ->
-        for (i in 0..<index1) {
+        for (i in index1 + 1..boxes.lastIndex) {
             val checkBox = boxes[i]
             connections.add(Connection(calculateDistance(box, checkBox), box, checkBox))
         }
     }
 
-    val circuits = mutableListOf<Circuit>()
+    return connections.sortedBy { it.distance }.toMutableList()
+}
 
-    connections = connections.sortedBy { it.distance }.toMutableList()
-
-    connections.forEach {
-        if (it.to.circuit == null && it.from.circuit == null) {
-            val newCircuit = Circuit(mutableSetOf(it.from, it.to))
-            it.from.circuit = newCircuit
-            it.to.circuit = newCircuit
-            circuits.add(newCircuit)
-        } else if (it.to.circuit != null && it.from.circuit == null) {
-            it.from.circuit = it.to.circuit
-            it.to.circuit!!.boxes.add(it.from)
-        } else if (it.from.circuit != null && it.to.circuit == null) {
-            it.to.circuit = it.from.circuit
-            it.from.circuit!!.boxes.add(it.to)
-        }
+private fun getBoxes(input: List<String>) = input.map {
+    it.split(",").let {
+        JunctionBox(
+            x = it[0].toLong(),
+            y = it[1].toLong(),
+            z = it[2].toLong()
+        )
     }
-
-    return circuits.sortedByDescending { it.boxes.size }.take(3).fold(1) { acc, circuit -> acc * circuit.boxes.size }
 }
 
 private data class Connection(
@@ -60,7 +98,7 @@ private data class Connection(
 )
 
 private data class Circuit(
-    val boxes: MutableSet<JunctionBox>
+    val boxes: List<JunctionBox>
 )
 
 private fun calculateDistance(p1: JunctionBox, p2: JunctionBox): Double {
@@ -71,12 +109,10 @@ private fun calculateDistance(p1: JunctionBox, p2: JunctionBox): Double {
 }
 
 private data class JunctionBox(
-    val x: Int,
-    val y: Int,
-    val z: Int
-) {
-    var circuit: Circuit? = null
-}
+    val x: Long,
+    val y: Long,
+    val z: Long
+)
 
 private fun getInput(): List<String> {
     val bufferedReader: BufferedReader = File("day-8-input.txt").bufferedReader()
